e1a2ff5a25192f88abc25ffcde5e18a649f73e9c
github username <github email address>
2018-08-22 10:56:29 -0400

    create renderAvatar and getAllAvatars in Avatar, add avatarId in User

--- Game-project-front-end/src/avatar.js
+++ Game-project-front-end/src/avatar.js
@@ -7,5 +7,36 @@ class Avatar{
     gameStore.avatars.push(this)
   }
 
+  static getAllAvatars(){
+    gameStore.avatars.forEach(avatar=>{avatar.renderAvatar()})
+  }
+
+  static findAvatar(id){
+    return gameStore.avatars.find(avatar=> {return avatar.id === id})
+  }
+
+  renderAvatar(){
+    let img = document.createElement('img')
+    let h4 = document.createElement('h4')
+    let p = document.createElement('p')
+    let divCard = document.createElement('div')
+    let divImage = document.createElement('div')
+    let divHeader = document.createElement('div')
+    let divDescription = document.createElement('div')
+
+    divHeader.classList.add('header')
+    divDescription.classList.add('description')
+    p.innerHTML = this.description
+    img.src = this.image
+    h4.innerHTML = this.name
+    divImage.classList.add('image')
+    divImage.append(img)
+    divCard.id = `avatar-${this.id}`
+    divCard.classList.add('card')
+    divHeader.append(h4)
+    divDescription.append(p)
+    divCard.append(divImage, divHeader, divDescription)
+    divContainer.append(divCard)
+  }
 
 }
--- Game-project-front-end/src/index.js
+++ Game-project-front-end/src/index.js
@@ -1,6 +1,5 @@
 const divContainer = document.querySelector('#fighter-container')
 
-
 document.addEventListener('DOMContentLoaded', ()=>{
   renderButtons()
   getUsers()
@@ -11,13 +10,14 @@ function getUsers() {
   fetch('http://localhost:3000/users')
   .then(res=> res.json())
   .then(json=>{
-    //render
     json.forEach(element=>createUsers(element))
   })
 }
 
 function createUsers(element) {
-  let user = new User(element.id, element.name, element.username)
+  let id = []
+  element.avatars.forEach(avatar=> {id.push(avatar.id)})
+  let user = new User(element.id, element.name, element.username, id)
 }
 
 function getAvatars() {
@@ -32,37 +32,19 @@ function getAvatars() {
 
 function createAvatars(element) {
   let avatar = new Avatar(element.id, element.name, element.image, element.description)
-  let img = document.createElement('img')
-  let h4 = document.createElement('h4')
-  let p = document.createElement('p')
-  let divCard = document.createElement('div')
-  let divImage = document.createElement('div')
-  let divHeader = document.createElement('div')
-  let divDescription = document.createElement('div')
-
-  divHeader.classList.add('header')
-  divDescription.classList.add('description')
-  p.innerHTML = element.description
-  img.src = element.image
-  h4.innerHTML = element.name
-  divImage.classList.add('image')
-  divImage.append(img)
-  divCard.id = `avatar-${element.id}`
-  divCard.classList.add('card')
-  divHeader.append(h4)
-  divDescription.append(p)
-  divCard.append(divImage, divHeader, divDescription)
-  divContainer.append(divCard)
+  avatar.renderAvatar()
 }
 
 function renderButtons() {
   const buttonDiv = document.querySelector('#buttonsDiv')
   let loginBtn = document.createElement('button')
   let accBtn = document.createElement('button')
+
   loginBtn.classList.add('ui', 'button')
   accBtn.classList.add('ui', 'positive', 'button')
   loginBtn.innerText = 'Login'
   accBtn.innerText = 'Create Account'
+
   loginBtn.addEventListener('click', renderLogin)
   accBtn.addEventListener('click', renderAccount)
   buttonDiv.append(loginBtn, accBtn)
@@ -73,6 +55,7 @@ function renderLogin() {
   let usernameInput = document.createElement('input')
   let passwordInput = document.createElement('input')
   let submitBtn = document.createElement('submit')
+
 }
 
 function renderAccount() {
